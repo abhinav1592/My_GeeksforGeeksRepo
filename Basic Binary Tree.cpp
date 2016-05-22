fd0b86b70b32fa9336edb0cdf11c52b47ee33711
Abhinav Shrivastava <abhinavisking@gmail.com>
2016-05-23 00:34:20 +0530

    Level Order Traversals with and without Queue

--- Basic Binary Tree.cpp
+++ Basic Binary Tree.cpp
@@ -1,6 +1,7 @@
-#include <bits/stdc++>
+#include <bits/stdc++.h>
 #define endl "\n"
 using namespace std;
+void printGivenLevel(struct node* root, int level);
 
 // Basic Tree Structure
 struct node
@@ -12,83 +13,124 @@ struct node
 
 struct node *newNode(int data)
 {
-      struct node *node = (struct node *)malloc(sizeof(struct node));
-      node->data = data;
-      node->left = NULL;
-      node->right = NULL;
-      return (node);
+    struct node *node = (struct node *)malloc(sizeof(struct node));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    return (node);
 }
 // Traversal - Pre,In,Post
-void preorder(struct node *node)
+void preorder(struct node *root)
 {
-	cout<<node->data<<endl;
-	preorder(node->left);
-	preorder(node->right);
+    if(root==NULL) return;
+    cout<<root->data<<" == ";
+    preorder(root->left);
+    preorder(root->right);
 }
 
-void inorder(struct node *node)
+void inorder(struct node *root)
 {
-	inorder(node->left);
-	cout<<node->data<<endl;
-	inorder(node->right);
+    if(root==NULL) return;
+    inorder(root->left);
+    cout<<root->data<<" == ";
+    inorder(root->right);
 }
 
 
-void postorder(struct node *node)
+void postorder(struct node *root)
 {
-	
-	postorder(node->left);
-	postorder(node->right);
-	cout<<node->data<<endl;
+    if(root==NULL) return;
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" == ";
 }
 
 // Height of Binary Tree
 
-int height(struct node *node)
+int height(struct node *root)
 {
-	if(node==NULL) return 0;
-	else
-	{
-		int left_subtree_height = height(node->left);
-		int right_subtree_height = height(node->right);
-
-		if(left_subtree_height>right_subtree_height)
-			return left_subtree_height+1;
-		else return right_subtree_height+1;
-	}
+    if(root==NULL) return 0;
+    else
+        {
+            int left_subtree_height = height(root->left);
+            int right_subtree_height = height(root->right);
+            if(left_subtree_height>right_subtree_height)
+                return left_subtree_height+1;
+            else return right_subtree_height+1;
+        }
 }
+
+// Level order traversal
+
+/* Function to print level order traversal a tree*/
+void level_order_without_queue(struct node *root)
+{
+    int h = height(root);
+    int i;
+    for (i=1; i<=h; i++)
+        printGivenLevel(root, i);
+}
+
+/* Print nodes at a given level */
+void printGivenLevel(struct node* root, int level)
+{
+    if (root == NULL)
+        return;
+    if (level == 1)
+        cout<< root->data<<" == ";
+    else if (level > 1)
+        {
+            printGivenLevel(root->left, level-1);
+            printGivenLevel(root->right, level-1);
+        }
+}
+
+
+/* Function to print level order traversal a tree (with queue)*/
+void level_order_with_queue(struct node *root)
+{
+    queue<struct node *> node_queue;
+    struct node *temp_node = root;
+    while(temp_node!=NULL)
+        {
+            cout<<temp_node->data<<" == ";
+            node_queue.push(temp_node->left);
+            node_queue.push(temp_node->right);
+            if(!node_queue.empty())
+                {
+                    temp_node = node_queue.front();
+                    node_queue.pop();
+                }
+            else
+                {
+                    temp_node=NULL;
+                }
+        }
+}
+
 int main()
 {
-	struct node *root = newNode(1);
-
-	root->left = newNode(2);
-	root->right = newNode(3);
-
-	root->left->left = newNode(4);
-	root->left->right = newNode(5);
-
-	root->right->left = newNode(6);
-	root->right->right = newNode(7);
+    struct node *root = newNode(1);
+    root->left = newNode(2);
+    root->right = newNode(3);
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+    root->right->left = newNode(6);
+    root->right->right = newNode(7);
     // ==================================================
     //Traversals
     cout<<"\n Preorder traversal : "<<endl;
     preorder(root);
-     cout<<"\n Inorder traversal : "<<endl;
+    cout<<"\n Inorder traversal : "<<endl;
     inorder(root);
     cout<<"\n Postorder traversal : "<<endl;
     postorder(root);
-    cout<<"\n Level Order Traversal(without Queue) : "
-    cout<<level_order_without_queue(root);
-    cout<<"\n Level Order Traversal(with Queue) : "
-    cout<<level_order_with_queue(root);
-    cout<<"\n Depth First Traversal(with stack) : "
-    cout<<depth_first_traversal_with_stack(root);
-    
+    cout<<"\n Level Order Traversal(without Queue) : "<<endl;
+    level_order_without_queue(root);
+    cout<<"\n Level Order Traversal(with Queue) : "<<endl;
+    level_order_with_queue(root);
     // ===================================================
-    
     // Height of tree
-    cout<<" Height of current tree : "<<height(root)<<endl;
-
-
-	return 0;
+    cout<<"\n Height of current tree : "<<height(root)<<endl;
+    return 0;
 }
